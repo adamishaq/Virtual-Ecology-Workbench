7df0f5c9c538b0692faae291e3d70bfc89d276de
Michael H <mh2409@doc.ic.ac.uk>
2011-10-21 14:10:38 +0100

    Moved some stuff around to make it compile in Eclipse

---
+++ bin/vew.bat
@@ -0,0 +1,1 @@
+java -classpath trove.jar;ganymed-ssh2.jar;tools.jar;netcdf.jar;jgrib.jar;freehep-io-2.0.1.jar;freehep-util-2.0.1.jar;freehep-graphics2d-2.0.jar;freehep-graphicsio-2.0.jar;freehep-graphicsio-ps-2.0.jar;dom4j-1.6.1.jar;jaxen-1.1-beta-9.jar;. -Xmx1000m VEWMain2
