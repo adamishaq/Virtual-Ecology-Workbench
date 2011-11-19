61a989b1a6ae6915f580b6e40e775e98541e4eb6
asw09 <andrew-west@hotmail.co.uk>
2011-11-19 11:42:08 +0000

    Variable editor now has full skeleton - I just need the model code to make it actually do stuff.

--- src/VEW/UIComponents/VariableEditorPanel.java
+++ src/VEW/UIComponents/VariableEditorPanel.java
@@ -4,15 +4,33 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.event.ListSelectionListener;
 
 public class VariableEditorPanel extends JPanel {
 
 	private Dimension d;
+	
+	JPanel variable_info = new JPanel(new GridBagLayout());
+	GridBagConstraints c = new GridBagConstraints();
+	JLabel type = new JLabel("Type/Scope :");
+	JComboBox  type_combo = new JComboBox();
+	JLabel name = new JLabel("Variable Name :   ");
+	JTextField var_name = new JTextField();
+	JLabel desc = new JLabel("Description :");
+	JTextField var_desc = new JTextField();
+	JLabel history_size = new JLabel("History Size :  ");
+	JTextField h_size = new JTextField();
+	JLabel initial_value = new JLabel("Initial Value :  ");
+	JTextField i_val = new JTextField();
+	JLabel fs_link = new JLabel("Food-Set Link :");
+	JComboBox  link_combo = new JComboBox();
 	
 	public VariableEditorPanel(Dimension dimension) {
 		this.setPreferredSize(dimension);
@@ -21,20 +39,15 @@ public class VariableEditorPanel extends JPanel {
 	}
 
 	private void initialize() {
-		JPanel variable_info = new JPanel(new GridBagLayout());
 		variable_info.setPreferredSize(new Dimension(d.width - 25, d.height - 25));
-		GridBagConstraints c = new GridBagConstraints();
-		
 		// Label for scope choice
 		c.ipady = 5;
-		JLabel type = new JLabel("Type/Scope :");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridwidth = 2;
 		c.gridx = 0;
 		c.gridy = 0;
 		variable_info.add(type, c);
 		// Combo box to choose scope
-		JComboBox  type_combo = new JComboBox();
 		type_combo.addItem("Group Variable");
 		type_combo.addItem("Group Parameter");
 		type_combo.addItem("Local Variable");
@@ -42,6 +55,7 @@ public class VariableEditorPanel extends JPanel {
 		type_combo.addItem("Food Set/Concentration");
 		type_combo.addItem("Food-Based Local");
 		type_combo.addItem("Food-Based Variable");
+		type_combo.addItemListener(new VarTypeListener(this));
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridwidth = 2;
 		c.gridx = 3;
@@ -49,61 +63,105 @@ public class VariableEditorPanel extends JPanel {
 		variable_info.add(type_combo, c);
 		
 		// Label for Variable name
-		JLabel name = new JLabel("Variable Name :   ");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
 		c.gridy = 1;
 		variable_info.add(name, c);
 		// Text field for Var name
-		JTextField var_name = new JTextField();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 3;
 		c.gridy = 1;
 		variable_info.add(var_name, c);
 		
 		// Label for Variable description
-		JLabel desc = new JLabel("Description :");
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 0;
 		c.gridy = 2;
 		variable_info.add(desc, c);
 		// Text field for Var description
-		JTextField var_desc = new JTextField();
 		c.fill = GridBagConstraints.HORIZONTAL;
 		c.gridx = 3;
 		c.gridy = 2;
 		variable_info.add(var_desc, c);
-		
-		// Label for History Size
-		JLabel history_size = new JLabel("History Size :  ");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridwidth = 1;
-		c.gridx = 0;
-		c.gridy = 3;
-		variable_info.add(history_size, c);
-		// Text field for History Size
-		JTextField h_size = new JTextField();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 1;
-		c.gridy = 3;
-		variable_info.add(h_size, c);
-		
-		// Label for Initial value
-		JLabel initial_value = new JLabel(" Initial Value :  ");
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 3;
-		c.gridy = 3;
-		variable_info.add(initial_value, c);
-		// Text field for Initial value
-		JTextField i_val = new JTextField();
-		c.fill = GridBagConstraints.HORIZONTAL;
-		c.gridx = 4;
-		c.gridy = 3;
-		variable_info.add(i_val, c);
-		
-		
-		
+
+		change_var_type("Group Variable");
 		this.add(variable_info,BorderLayout.WEST);
+	}
+	
+	public void change_var_type(String var_type) {
+		boolean i_value = true;
+		boolean history = true;
+		boolean food_set = true;
+		if (var_type.equals("Group Variable")) {
+			food_set = false;
+		} else if (var_type.equals("Group Parameter")) {
+			history = false;
+			food_set = false;
+		} else if (var_type.equals("Local Variable")) {
+			i_value = false;
+			history = false;
+			food_set = false;
+		} else if (var_type.equals("Food-Based Parameter")) {
+			history = false;
+		} else if (var_type.equals("Food Set/Concentration")) {
+			i_value = false;
+			history = false;
+			food_set = false;
+		} else if (var_type.equals("Food-Based Local")) {
+			i_value = false;
+			history = false;
+		} else if (var_type.equals("Food-Based Variable")) {
+
+		}
+		history_size.setVisible(false);
+		variable_info.remove(history_size);
+		variable_info.remove(h_size);
+		initial_value.setVisible(false);
+		variable_info.remove(initial_value);
+		variable_info.remove(i_val);
+		fs_link.setVisible(false);
+		variable_info.remove(fs_link);
+		variable_info.remove(link_combo);
+		variable_info.validate();
+		int y = 3;
+		if (history) {
+			// Label for History Size
+			history_size.setVisible(true);
+			c.gridx = 0;
+			c.gridy = y;
+			variable_info.add(history_size, c);
+			// Text field for History Size
+			c.gridx = 3;
+			c.gridy = y;
+			variable_info.add(h_size, c);
+			y++;
+		}
+		if (i_value) {
+			// Label for Initial value
+			initial_value.setVisible(true);
+			c.gridx = 0;
+			c.gridy = y;
+			variable_info.add(initial_value, c);
+			// Text field for Initial value=
+			c.gridx = 3;
+			c.gridy = y;
+			variable_info.add(i_val, c);
+			y++;
+		}
+		if (food_set) {
+			// Label for food-set link
+			fs_link.setVisible(true);
+			c.gridx = 0;
+			c.gridy = y;
+			variable_info.add(fs_link, c);
+			// Combo box for food links
+			link_combo.addItem("Food Set 1");
+			link_combo.addItem("Food Set 2");
+			c.gridx = 3;
+			c.gridy = y;
+			variable_info.add(link_combo, c);
+		}
+		variable_info.validate();
 	}
 
 	/**
@@ -111,6 +169,24 @@ public class VariableEditorPanel extends JPanel {
 	 */
 	private static final long serialVersionUID = 1484036714507019850L;
 
-	
+	static class VarTypeListener implements ItemListener {
+
+		private String current = "";
+		private VariableEditorPanel parent;
+		
+		public VarTypeListener(VariableEditorPanel par) {
+			this.parent = par;
+		}
+		
+		@Override
+		public void itemStateChanged(ItemEvent i) {
+			String selection = i.getItem().toString();
+			if (!current.equals(selection)) {
+				current = selection;
+				parent.change_var_type(selection);
+			}
+		}
+		
+	}
 	
 }
